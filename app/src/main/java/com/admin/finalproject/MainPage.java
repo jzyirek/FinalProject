9730c53f67db9d55a4d1d874abbfc8e2d9a4979e
BUSCIS-C1-L19 <BUSCIS-C1-L19@scc.ad.losrios.edu>
2017-11-08 16:36:27 -0800

    Initial commit

---
+++ app/src/main/java/com/admin/finalproject/MainPage.java
@@ -0,0 +1,64 @@
+package com.admin.finalproject;
+
+import android.content.DialogInterface;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Spinner;
+
+public class MainPage extends AppCompatActivity implements OnClickListener
+{
+    //buttons
+    private Button StartButtonId;
+
+    //text views
+
+    //spinner
+    private Spinner GamesSpinnerId;
+
+    //radio buttons
+    private RadioButton OnePlayerRadioId;
+    private RadioButton TwoPlayerRadioId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        StartButtonId = (Button) findViewById(R.id.StartButtonId);
+
+        OnePlayerRadioId = (RadioButton) findViewById(R.id.OnePlayerRadioId);
+        TwoPlayerRadioId = (RadioButton) findViewById(R.id.TwoPlayerRadioId);
+
+        GamesSpinnerId = (Spinner) findViewById(R.id.GamesSpinnerId);
+
+        this.StartButtonId.setOnClickListener(this);
+        Init();
+    }
+
+    private void Init()
+    {
+        OnePlayerRadioId.setClickable(false);
+        TwoPlayerRadioId.setChecked(true);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.games_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        GamesSpinnerId.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
---
+++ app/src/main/java/com/admin/finalproject/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.admin.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicTacToe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe);
+    }
+}
